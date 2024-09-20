818b88fd4f7c1ec9f6619eb9a7e42f20aaaf3207
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-09-20 00:05:14 -0500

    First commitment on creating todolist

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+ <div>
+  <h1>My To-Do List</h1>
+  <input v-model="newTodo" @keyupenter="addTodo" placeholder="Add a new task" />
+ <ul>
+  <TodoItem
+  v-for="(todo, index) in todos"
+  :key="index"
+  :todo="todo"
+  @remove="removeTodo(index)"
+  />
+ </ul>
+ </div>
+</template>
+<script>
+
+
+</script>
+<style scoped>
+
+</style>
