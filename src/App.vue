07e1afb046d0deba5fa7081f50d9050745dc7b05
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-09-20 00:39:20 -0500

    able to add , and remove things to do

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const newTodo = ref(''); // Holds the value of the input
+const todos = ref([]);   // Holds the list of todos
+
+// Method to add a new todo item
+const addTodo = () => {
+  if (newTodo.value.trim()) { // Check if the input is not empty
+    todos.value.push({ text: newTodo.value, completed: false }); // Add new todo
+    newTodo.value = ''; // Clear the input field
+  }
+};
+
+// Method to remove a todo item
+const removeTodo = (index) => {
+  todos.value.splice(index, 1); // Remove todo at the specified index
+};
 </script>
 
 <template>
- <div>
-  <h1>My To-Do List</h1>
-  <input v-model="newTodo" @keyupenter="addTodo" placeholder="Add a new task" />
- <ul>
-  <TodoItem
-  v-for="(todo, index) in todos"
-  :key="index"
-  :todo="todo"
-  @remove="removeTodo(index)"
-  />
- </ul>
- </div>
+  <div>
+   
+    <h1>My To-Do List</h1>
+    <input
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+      placeholder="Add a new task"
+    />
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <input type="checkbox" v-model="todo.completed" />
+        <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
 </template>
-<script>
 
 
-</script>
-<style scoped>
-
-</style>
